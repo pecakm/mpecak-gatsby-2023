c2699f547d7832e10a219260f7baad7f3f463efa
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-21 22:06:32 +0100

    Add year to projects

--- src/content/projekty.content.ts
+++ src/content/projekty.content.ts
@@ -1,79 +1,79 @@
 export const CodeProjects = [{
   path: 'https://ib-raport.pl',
-  title: 'IB Raport',
+  title: 'IB Raport (2022)',
   description: 'Aplikacja raportująca dla przedsiębiorstwa (React / Firebase)',
 }, {
   path: 'https://mpecak.pl',
-  title: 'mpecak.pl',
+  title: 'mpecak.pl (2023)',
   description: 'Strona internetowa (GatsbyJS)',
 }, {
   path: 'https://bitbucket.org/pecakm/wheretogo-frontend',
-  title: 'Pulcito! - Frontend',
+  title: 'Pulcito! - Frontend (2018)',
   description: 'Aplikacja rozrywkowa na potrzeby własnych ćwiczeń (Angular 6)',
 }, {
   path: 'https://bitbucket.org/pecakm/wheretogo-backend',
-  title: 'Pulcito! - Backend',
+  title: 'Pulcito! - Backend (2018)',
   description: 'Aplikacja rozrywkowa na potrzeby własnych ćwiczeń (Express.js)',
 }, {
   path: 'https://bitbucket.org/pecakm/ai-tut',
-  title: 'Trening AI',
+  title: 'Trening AI (2019)',
   description: 'Studencki projekt sztucznej inteligencji (ReactJS)',
 }, {
   path: 'https://bitbucket.org/pecakm/ekspresowakawa',
-  title: 'Ekspresowa Kawa',
+  title: 'Ekspresowa Kawa (2018)',
   description: 'Aplikacja internetowa do zamawiania kawy (ReactJS / Symfony 4)',
 }, {
   path: 'https://bitbucket.org/pecakm/dailynote',
-  title: 'DailyNote Web',
+  title: 'DailyNote Web (2017)',
   description: 'Aplikacja internetowa do zapisywania notatek (JS / Firebase)',
 }, {
   path: 'https://bitbucket.org/pecakm/dailynote-ios',
-  title: 'DailyNote iOS',
+  title: 'DailyNote iOS (2017)',
   description: 'Aplikacja mobilna do zapisywania notatek (Swift / Firebase)',
 }, {
   path: 'https://bitbucket.org/pecakm/citylist-backend',
-  title: 'City List Backend',
+  title: 'City List Backend (2018)',
   description: 'Backend do ćwiczeń własnych (Express.js / MongoDB)',
 }, {
   path: 'https://bitbucket.org/pecakm/pushthebutton-php',
-  title: 'Push The Button',
+  title: 'Push The Button (2018)',
   description: 'Gra przeglądarkowa (PHP)',
 }, {
   path: 'https://bitbucket.org/pecakm/ang-game',
-  title: 'Ang-Game',
+  title: 'Ang-Game (2019)',
   description: 'Próba stworzenia gry przeglądarkowej (Angular 6)',
 }];
 
 export const WordPressProjects = [{
-  path: 'http://bernacik.pl',
+  path: 'http://bernacik.pl (2017)',
   title: 'bernacik.pl',
   description: 'Strona internetowa - Kierowca wyścigowy',
 }, {
-  path: 'https://dynamech.pl',
+  path: 'https://dynamech.pl (2017)',
   title: 'dynamech.pl',
   description: 'Strona internetowa - Diagnostyka i naprawa turbosprężarek',
 }, {
-  path: 'http://wtr.com.pl',
+  path: 'http://wtr.com.pl (2018)',
   title: 'WTR Motorsport',
   description: 'Strona internetowa - Zespół wyścigowy',
 }, {
-  path: 'http://arthouse-studio.pl',
+  path: 'http://arthouse-studio.pl (2018)',
   title: 'Art House Studio',
   description: 'Strona internetowa - Projektowanie wnętrz i meble kuchenne',
 }, {
-  path: 'https://itpulse.pl/interbell',
+  path: 'https://itpulse.pl/interbell (2017)',
   title: 'Interbell',
   description: 'Strona internetowa - Propozycja odnowy strony przedsiębiorstwa',
 }, {
   path: 'https://apartamentynowoczesne.pl',
-  title: 'Apartamenty Nowoczesne',
+  title: 'Apartamenty Nowoczesne (2018)',
   description: 'Strona internetowa - Sprzedaż nieruchomości',
 }, {
   path: 'https://gostynapartamenty.pl',
-  title: 'Gostyń Apartamenty',
+  title: 'Gostyń Apartamenty (2020)',
   description: 'Strona internetowa - Sprzedaż nieruchomości',
 }, {
   path: 'https://janikowonadjeziorem.pl',
-  title: 'Janikowo nad jeziorem',
+  title: 'Janikowo nad jeziorem (2019)',
   description: 'Strona internetowa - Sprzedaż nieruchomości',
 }];
